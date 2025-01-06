1f4cf44af454d5de622293a98eca6d5bbc6f99d7
Omer-Sait-Kemaloglu <omersaitkemaloglu@gmail.com>
2025-01-06 14:12:07 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // Test örnekleri
-        System.out.println(shouldWakeUp(true, 1)); // true
-        System.out.println(shouldWakeUp(false, 2)); // false
-        System.out.println(shouldWakeUp(true, 8)); // false
-        System.out.println(shouldWakeUp(true, -1)); // false
 
-        System.out.println(hasTeen(9, 99, 19)); // true
-        System.out.println(hasTeen(23, 15, 42)); // true
-        System.out.println(hasTeen(22, 23, 34)); // false
-
-        System.out.println(isCatPlaying(true, 10)); // false
-        System.out.println(isCatPlaying(false, 36)); // false
-        System.out.println(isCatPlaying(false, 35)); // true
-
-        System.out.println(area(5.0, 4.0)); // 20.0
-        System.out.println(area(-1.0, 4.0)); // -1.0
-        System.out.println(area(5.0)); // 78.53975...
-        System.out.println(area(-1.0)); // -1.0
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
